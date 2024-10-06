aef0113144dd12cb6a7610ea0bbcfccc3d672542
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-10-06 07:09:39 +0300

    update clean code server and initMongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+
 import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
 
